3dc7d3bddf1a885227922e00ee27d911ad1e9ada
Chris Clifford <cclifford82@gmail.com>
2016-12-16 12:26:40 -0800

    add html and routing

--- app.rb
+++ app.rb
@@ -8,10 +8,49 @@ get('/') do
   erb(:index)
 end
 
+get('/stores/new') do
+  @stores = Store.all()
+  @show = ''
+  erb(:index)
+end
+
 get('/stores/:id') do
   @store = Store.find(params['id'])
   erb(:store)
 end
+
+post('/stores') do
+  name = params['store-name']
+  street = params['store-street']
+  city = params['store-city']
+  state = params['store-state']
+  if Store.create(:name => name, :city => city, :street => street, :state => state)
+    redirect('/')
+  else
+    erb(:error)
+  end
+end
+
+get('/shoes/:id/new') do
+  @store = Store.find(params['id'])
+  @show = ''
+  erb(:store)
+end
+
+post('/shoes/:id') do
+  @store = Store.find(params['id'])
+  name = params['shoe-name']
+  brand = params['shoe-brand']
+  style = params['shoe-style']
+  price = params['shoe-price']
+  description = params['shoe-description']
+  if @store.shoes.create(:name => name, :brand => brand, :description => description, :style => style, :cost => price)
+    redirect("/stores/#{@store.id()}")
+  else
+    erb(:error)
+  end
+end
+
 
 
 
